968a097d074f372ddfba38679d3658781956b9dd
sj-3821 <sujalkumar3821@gmail.com>
2024-10-26 21:43:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from the other side');
+  res.send('Hello World from the server side');
 });
 
 module.exports = app;
